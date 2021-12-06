e771a41c292437f16de73b615cc69c2d07a86f68
Koorineroki <60882222+Koorineroki@users.noreply.github.com>
2021-12-06 20:08:16 +0800

    Add files via upload

--- src/RainChen.ino
+++ src/RainChen.ino
@@ -1,60 +1,146 @@
-//红外声明
 #include <IRremote.h>
-IRrecv irrecv(11);
+#include <Servo.h>
+
+//IR Setup
+int IR_PIN = 11;
+IRrecv irrecv(IR_PIN);
 decode_results results;
 
-//舵机声明
-#include <Servo.h>
-Servo myservo;
+//Servo Setup
+Servo myservo_01;
+Servo myservo_02;
+int Servo_01_PIN = 9;
+int Servo_02_PIN = 5;
+int angle_01 = 0;
+int angle_02 = 10;
 
-//雨水传感器声明
+//RainSensor Setup
 #define rainfall A0
-int value;
+int rain_value;
 int set=110;
+bool rain = false;
+bool keep = false;
 
-
+//Stage keep LED Setup
+int LED = 3;
 void setup()
 {
-  //关联端口
-  myservo.attach(9);
+  //Port
+  myservo_01.attach(Servo_01_PIN);
+  myservo_02.attach(Servo_02_PIN);
   irrecv.enableIRIn();
   pinMode(rainfall,INPUT);
 
-  //测试专用
+  //Test
   Serial.begin(9600);
+
+  pinMode(LED,OUTPUT);
 }
 
-void loop() {
-  
-  static int a =0;
-  value = map(analogRead(rainfall),0,1023,0,255);
 
-  Serial.println(value);
+void loop()
+{
+  //Map the value
+  rain_value = map(analogRead(rainfall),0,1023,0,255);
+  Serial.println(rain_value);
 
-  if (irrecv.decode(&results)) {
+
+  //When Get IR Signal
+  if (irrecv.decode(&results)) 
+  {
     irrecv.resume();
 
-     //开窗户
-    if(results.value==16718055 && value>=set)
+    int signol = results.value;
+    switch(signol){
+      //Keep rain stage
+      case 16738455 :
+        keep = true;
+        rain = false;
+        digitalWrite(LED,HIGH);
+        Serial.println("rain stage is KEEP FASLE!");
+        break;
+        
+      case 16756815 :
+        keep = false;
+        Serial.println("rain stage is NOT KEEP");
+        digitalWrite(LED,LOW);
+        break;
+
+
+      //Open ALL windows
+      case 16718055 :
+        if (!rain)
+        {
+          angle_01 = 60;
+          angle_02 = 70;
+        }
+        break;
+
+      //Close ALL windows
+      case 16730805 :
+        angle_01 = 0;
+        angle_02 = 10;
+        break;
+
+        
+      //Open window 1
+      case 16753245 :
+        if (!rain)
+        {
+          angle_01 = 60;
+        }
+        break;
+        
+      //Open window 2
+      case 16720605 :
+        if (!rain)
+        {
+          angle_02 = 70;
+        }
+        break;
+
+
+      //Close window 1
+      case 16769565 :
+        if (!rain)
+        {
+          angle_01 = 0;
+        }
+        break;
+
+      //Close window 2
+      case 16761405 :
+        if (!rain)
+        {
+          angle_02 = 10;
+        }
+        break;
+      }
+      
+
+    //Satge of rain
+    if (!keep)
     {
-      a = 60;
+      if (rain_value >= set)
+      {
+        rain = false;
+      }
+      else if (rain_value < set)
+      {
+        rain = true;
+      }
     }
 
-    //关窗户
-    if(results.value==16730805)
-    {
-      a = 0;
-    }
-  }
-
-    //自动关窗
-    if(value<set)
+    //Auto close
+    if(rain_value<set && rain)
     {
       Serial.println("rain detected");
-      a = 0;
+      angle_01 = 0;
+      angle_02 = 10;
     }
   
-  
-  myservo.write(a);
+  myservo_01.write(angle_01);
+  myservo_02.write(angle_02);
   delay(200);
+  }
 }
